50474f86ee6a88323e2737e14af764456fe8c8ec
Olia <good-friday@mail.ru>
2021-09-16 22:59:23 +0300

    Доработка стилей и скрипта

--- source/js/modules/loader.js
+++ source/js/modules/loader.js
@@ -1,49 +1,54 @@
 const TABLET_WIDTH = 1023;
 
 const header = document.querySelector('.header');
+header.classList.add('header--loader');
+
+const loader = document.querySelector('.header--loader');
 const loaderText = document.querySelector('.header__text');
 const introHeading = document.querySelector('.intro__heading');
 const details = document.querySelector('.details');
 
-header.classList.add('header--loader');
+const logoLink = document.querySelector('.header__logo');
+logoLink.setAttribute('tabindex', -1);
 
 if (document.body.clientWidth > TABLET_WIDTH) {
-  loaderText.innerText = 'Нажмите на Enter';
-  const onEnterKeydown = (evt) => {
-    if (evt.key === ('Enter')) {
-      evt.preventDefault();
-      header.classList.remove('header--loader');
-      header.classList.add('header--closed');
+  if (loader) {
+    loaderText.innerText = 'Нажмите на Enter';
 
-      introHeading.classList.add('intro__heading--opened');
-      details.classList.add('details--opened');
-      window.removeEventListener('keydown', onEnterKeydown);
-    }
-  };
-
-  window.addEventListener('keydown', onEnterKeydown);
+    const onEnterKeydown = (evt) => {
+      if (evt.key === ('Enter')) {
+        evt.preventDefault();
+        logoLink.removeAttribute('tabindex');
+        header.classList.remove('header--loader');
+        introHeading.classList.add('intro__heading--animated');
+        details.classList.add('details--animated');
+        window.removeEventListener('keydown', onEnterKeydown);
+      }
+    };
+    window.addEventListener('keydown', onEnterKeydown);
+  }
 }
 
 if (document.body.clientWidth <= TABLET_WIDTH) {
   loaderText.innerText = 'Кликните на экран';
 
-  const loader = document.querySelector('.header--loader');
   if (loader) {
     const onLoaderClick = (evt) => {
       evt.stopPropagation();
       header.classList.remove('header--loader');
-      header.classList.add('header--closed');
-      introHeading.classList.add('intro__heading--opened');
+      introHeading.classList.add('intro__heading--animated');
       loader.removeEventListener('click', onLoaderClick);
     };
-
     loader.addEventListener('click', onLoaderClick);
   }
 
-  document.addEventListener('click', (evt) => {
-    if (evt.target.closest('.header__logo')) {
-      return;
-    }
-    details.classList.toggle('details--blue-opened');
-  });
+  if (details) {
+    details.classList.remove('details--nojs');
+    window.addEventListener('click', (evt) => {
+      if (evt.target.closest('.header__logo')) {
+        return;
+      }
+      details.classList.toggle('details--slide');
+    });
+  }
 }
