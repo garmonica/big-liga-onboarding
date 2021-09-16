0adaa90d89db7eafedca725bf65e09933da0096d
Olia <good-friday@mail.ru>
2021-09-17 01:23:30 +0300

    Добавила тач событие

--- source/js/modules/details-slide.js
+++ source/js/modules/details-slide.js
@@ -3,10 +3,13 @@ import {TABLET_WIDTH} from './loader';
 const details = document.querySelector('.details');
 
 if (document.body.clientWidth <= TABLET_WIDTH && details) {
-  document.body.addEventListener('click', (evt) => {
+  const onScreenClick = (evt) => {
     if (evt.target.closest('.header__logo')) {
       return;
     }
     details.classList.toggle('details--slide');
-  });
+  };
+
+  window.addEventListener('click', onScreenClick);
+  window.addEventListener('touchmove', onScreenClick);
 }
